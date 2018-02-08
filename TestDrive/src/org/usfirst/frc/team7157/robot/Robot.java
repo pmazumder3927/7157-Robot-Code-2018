5b62f40eddb3c8fcfd882746a4b68a34f0df5939
Danny Yoo <elitenick57@gmail.com>
2018-02-07 22:49:05 -0800

    actual teleop

---
+++ TestDrive/src/org/usfirst/frc/team7157/robot/Robot.java
@@ -0,0 +1,131 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team7157.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.properties file in the
+ * project.
+ */
+public class Robot extends IterativeRobot {
+	private static final String kDefaultAuto = "Default";
+	private static final String kCustomAuto = "My Auto";
+	private String m_autoSelected;
+	private SendableChooser<String> m_chooser = new SendableChooser<>();
+	public static OI oi = new OI();
+	private TeleOpDrive tpd = new TeleOpDrive();
+	private int mode = 1;
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		m_chooser.addDefault("Default Auto", kDefaultAuto);
+		m_chooser.addObject("My Auto", kCustomAuto);
+		SmartDashboard.putData("Auto choices", m_chooser);
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * <p>You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+		m_autoSelected = m_chooser.getSelected();
+		// autoSelected = SmartDashboard.getString("Auto Selector",
+		// defaultAuto);
+		System.out.println("Auto selected: " + m_autoSelected);
+	}
+
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		switch (m_autoSelected) {
+			case kCustomAuto:
+				// Put custom auto code here
+				break;
+			case kDefaultAuto:
+			default:
+				// Put default auto code here
+				break;
+		}
+	}
+
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() {
+		assignButtons();
+		tpd.Drive(oi.m_driveStick);
+		tpd.percentSpeed = (int) oi.m_driveStick.getRawAxis(3);
+		SmartDashboard.putNumber("Cimcoder1", tpd.cimCoder.getDistance());
+		SmartDashboard.putNumber("Cimcoder2", tpd.cimCoder2.getDistance());
+		SmartDashboard.putNumber("percentSpeed", tpd.percentSpeed);
+	}
+	
+	public void telopInit() {
+		tpd.TeleDriveInit();
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+	
+	public void assignButtons() {
+		//start main talons
+		if(oi.m_driveStick.getRawButton(5)) {
+			tpd.Start();
+		}
+		//stop main talons
+		if(oi.m_driveStick.getRawButton(3)) {
+			tpd.Stop();
+		}
+		//intake
+		if(oi.m_driveStick.getTrigger() && mode == 1) {
+			tpd.intakeSpark.set(0);
+			tpd.intakeSpark.set(0);
+		}
+		//shooter
+		if(oi.m_driveStick.getTrigger() && mode == 0) {
+			tpd.StartShooter();
+		}
+		if(oi.m_driveStick.getRawButton(6) && mode == 1) {
+			mode = 0;
+		}
+		if(oi.m_driveStick.getRawButton(6) && mode == 0) {
+			mode = 1;
+		}
+		//stop everything
+		if(oi.m_driveStick.getRawButton(4)) {
+			tpd.ForceStop();
+		}
+	}
+}
