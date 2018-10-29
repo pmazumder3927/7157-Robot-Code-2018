c54cf6474bb2b4da30ee77e000e1f0e1350a04a9
Pramit <pmazumder3927@gmail.com>
2018-10-28 19:29:39 -0700

    Fixed Robot Pose
    thx roth

--- AutoTest/src/main/java/org/usfirst/frc/team7157/auto/RobotPose.java
+++ AutoTest/src/main/java/org/usfirst/frc/team7157/auto/RobotPose.java
@@ -1,23 +1,14 @@
-package org.usfirst.frc.team7157.auto;
-
-import org.usfirst.frc.team7157.drive.MainDrive;
-import org.usfirst.frc.team7157.robot.Robot;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import jaci.pathfinder.Pathfinder;
 
 public class RobotPose {
     private volatile double x, y, theta;
-    private volatile double currentDistance, lastDistance, deltaDistance, lastTheta, deltaX, deltaY;
-    private MainDrive _drive;
+    private volatile double lastDistance, deltaDistance, lastTheta, deltaX, deltaY;
+
     public RobotPose(){
         this.x = 0;
         this.y = 0;
         this.theta = 0;
-        _drive = Robot.Drive;
 
-        this.currentDistance = _drive.getAvgEncoder();
-        this.lastDistance = _drive.getAvgEncoder();
+        this.lastDistance =0;
         this.deltaDistance = 0;
     }
 
@@ -29,85 +20,26 @@ public class RobotPose {
         return y;
     }
 
-    public double getTheta() {
-        return theta % (2 * Math.PI);
+    public void updatePose(double distance, double gyro) {
+        deltaDistance = distance - lastDistance;
+        theta = Math.toRadians(gyro);
+
+        //Smallest angle between two angles
+        double dTheta = Math.atan2(Math.sin(theta - lastTheta), Math.cos(theta - lastTheta));
+
+        //Find change in position relative to robot
+        //(The robot moved this amount if you are looking from the POV of the robot)
+        //The X and Y here are not representative of the coordinate system
+        //Swapping X and Y here requires swapping X and Y below
+        deltaX = Math.cos(dTheta / 2) * deltaDistance;
+        deltaY = Math.sin(dTheta / 2) * deltaDistance;
+
+        //Swap x and y to swap the two axises;
+        //Rotate change in x and y to robot's old angle
+        x += deltaX * Math.cos(lastTheta) - deltaY * Math.sin(lastTheta);
+        y += deltaX * Math.sin(lastTheta) + deltaY * Math.cos(lastTheta);
+
+        lastDistance = distance;
+        lastTheta = theta;
     }
-
-    public synchronized void addX(double x){
-        this.x += x;
-    }
-
-    public synchronized void addY(double y){
-        this.y += y;
-    }
-
-    public synchronized void setTheta(double theta){
-        lastTheta = this.theta;
-        this.theta = theta;
-    }
-
-    public synchronized void setX(double x){
-      this.x = x;
-    }
-
-    public synchronized void setY(double y){
-      this.y = y;
-    }
-
-    public double getCurrentDistance() {
-        return currentDistance;
-    }
-
-    public void setCurrentDistance(double currentDistance) {
-        this.currentDistance = currentDistance;
-    }
-
-    public double getLastDistance() {
-        return lastDistance;
-    }
-
-    public void setLastDistance(double lastDistance) {
-        this.lastDistance = lastDistance;
-    }
-
-    public double getDeltaDistance() {
-        return deltaDistance;
-    }
-
-    public void setDeltaDistance(double deltaDistance) {
-        this.deltaDistance = deltaDistance;
-    }
-
-    public String toString(){
-        return "X Position: " + x + " Y Position: " + y + " Heading: " + theta;
-    }
-
-    public double getLastTheta() {
-        return lastTheta;
-    }
-
-    public void calculateDpos() {
-        double d = 1/2*(lastTheta - theta);
-    }
-
-    public void updatePose() {
-        setCurrentDistance(Robot.Drive.getAvgEncoder());
-        setDeltaDistance(getCurrentDistance() - getLastDistance());
-        
-        setTheta(Math.toRadians(Pathfinder.boundHalfDegrees(Robot.Drive.getAngle())));
-
-        SmartDashboard.putNumber("X", getX());
-        SmartDashboard.putNumber("Y", getY());
-        
-        double dTheta = getTheta() - lastTheta;
-
-        deltaX = Math.sin(dTheta) * getDeltaDistance();
-        deltaY = Math.cos(dTheta) * getDeltaDistance();
-        //rotates x and y around oldangle
-        addX(deltaX*Math.cos(getTheta()) - deltaY*Math.sin(getTheta()));
-        addY(deltaX*Math.sin(getTheta()) + deltaY*Math.cos(getTheta()));
-
-        setLastDistance(getCurrentDistance());
-    }
-    
 }
